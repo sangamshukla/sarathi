9f50cf2565ed93f0e653c148190f31838be31592
Anand <anandkshuklap@gmail.com>
2021-01-24 14:30:42 +0530

    First cmmit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Sarathi Foundation</title>
+</head>
+<body>
+    <div class="w-full flex bg-gray-100 p-3">
+        <div class="mx-auto"><p>Email: sarathifoudation@gmail.com</p></div>
+        <div class="mx-auto">Phone Number: +91-99-98-8-8-8-8 </div>
+    </div>
+    <div class="flex bg-red-500 text-white text-lg uppercase">
+        <ul class="flex flex-row">
+            <li class="px-5 cursor-pointer border-r border-white py-2 hover:bg-white hover:text-red-500">Home</li>
+            <li class="px-5 cursor-pointer border-r border-white py-2 hover:bg-white hover:text-red-500">About</li>
+            <li class="px-5 cursor-pointer border-r border-white py-2 hover:bg-white hover:text-red-500">Contact</li>
+            <li class="px-5 cursor-pointer border-r border-white py-2 hover:bg-white hover:text-red-500">What we do</li>
+            <li class="px-5 cursor-pointer border-r border-white py-2 hover:bg-white hover:text-red-500">Donate Us</li>
+            <li class="px-5 cursor-pointer border-r border-white py-2 hover:bg-white hover:text-red-500">Volunteer</li>
+
+        </ul>
+    </div>
+    <div class="slider">
+        <img src="" alt="" srcset="">
+        <img src="" alt="" srcset="">
+        <img src="" alt="" srcset="">
+        <img src="" alt="" srcset="">
+        <img src="" alt="" srcset="">
+    </div>
+</body>
+</html>
